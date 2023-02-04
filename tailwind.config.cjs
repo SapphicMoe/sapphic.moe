cdb211c2c963372c4589f9b92e5b62607107bea0
Chloe <solely@riseup.net>
2023-02-04 23:35:56 +0500

    fix: link hovering, props, etc

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,md,mdx,ts}'],
-  plugins: [
-    require('@catppuccin/tailwindcss')({
-      prefix: 'ctp',
-      defaultFlavour: 'mocha',
-    }),
-    require('@tailwindcss/typography'),
-  ],
   theme: {
     fontFamily: {
       body: ['Inter', 'sans-serif'],
@@ -23,6 +16,22 @@ module.exports = {
       'parallel': '#CA2917',
     },
     extend: {
+      typography: () => ({
+        invert: {
+          css: {
+            a: {
+              background: '#f5c2e7',
+              backgroundClip: 'text',
+              color: 'transparent',
+            },
+            'a:hover': {
+              background: '#c99fbe',
+              backgroundClip: 'text',
+              color: 'transparent',
+            },
+          },
+        },
+      }),
       fontSize: {
         '3xl': [
           '1.8rem',
@@ -33,4 +42,11 @@ module.exports = {
       },
     },
   },
+  plugins: [
+    require('@catppuccin/tailwindcss')({
+      prefix: 'ctp',
+      defaultFlavour: 'mocha',
+    }),
+    require('@tailwindcss/typography'),
+  ],
 };
