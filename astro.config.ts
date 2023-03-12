c333ac20072c21beec8a3f1b25c3933255c3096b
Chloe <solely@riseup.net>
2023-03-12 12:47:00 +0500

    fix: mark temp changes with 'TEMP', mdx build issue

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import type { AstroIntegration } from 'astro';
 
 import mdx from '@astrojs/mdx';
 import prefetch from '@astrojs/prefetch';
@@ -6,7 +7,6 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 import compress from 'astro-compress';
-
 import autolinkHeadings from 'rehype-autolink-headings';
 import externalLinks from 'rehype-external-links';
 import slug from 'rehype-slug';
@@ -14,24 +14,27 @@ import slug from 'rehype-slug';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://arciniega.one',
+  markdown: {
+    rehypePlugins: [
+      slug,
+      autolinkHeadings,
+      [
+        externalLinks,
+        {
+          target: '_blank',
+          rel: ['nofollow', 'noopener'],
+        },
+      ],
+    ],
+  },
   integrations: [
     tailwind(),
-    mdx({
-      rehypePlugins: [
-        slug,
-        autolinkHeadings,
-        [
-          externalLinks,
-          {
-            target: '_blank',
-            rel: ['nofollow', 'noopener'],
-          },
-        ],
-      ],
-    }),
     prefetch(),
     sitemap(),
     compress(),
+
+    // TEMP: The recent Astro update broke integration logic, so "AstroIntegration" has been supplied here for now.
+    mdx() as AstroIntegration,
   ],
   output: 'server',
   adapter: vercel(),
