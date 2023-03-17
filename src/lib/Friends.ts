9559c76673c6062e89e2c9b8fb68a5d13e915f79
Chloe <solely@riseup.net>
2023-03-17 23:20:40 +0500

    refactor: change webring logic

---
+++ src/lib/Friends.ts
@@ -0,0 +1,70 @@
+type Webring = {
+  name: string;
+  link: string;
+  image: string;
+}[];
+
+const Friends: Webring = [
+  {
+    name: 'Ovyerus',
+    link: 'https://ovyerus.com',
+    image: 'ovyerus.png',
+  },
+  {
+    name: 'espi',
+    link: 'https://espi.me',
+    image: 'espi.png',
+  },
+  {
+    name: 'Adryd',
+    link: 'https://adryd.com',
+    image: 'adryd.png',
+  },
+  {
+    name: 'erin',
+    link: 'https://e2.pm',
+    image: 'erin.png',
+  },
+  {
+    name: 'lewisakura',
+    link: 'https://lewisakura.moe',
+    image: 'lewi.png',
+  },
+  {
+    name: 'UtsuhoRocks',
+    link: 'https://utsuho.rocks',
+    image: 'utsuho.png',
+  },
+  {
+    name: 'Cynthia',
+    link: 'https://c7.pm',
+    image: 'cynthia.png',
+  },
+  {
+    name: 'Vin',
+    link: 'https://vin.pet',
+    image: 'vin.png',
+  },
+  {
+    name: 'Spotlight',
+    link: 'https://joscomputing.space',
+    image: 'spotlight.gif',
+  },
+  {
+    name: 'Megumin',
+    link: 'https://megu.dev',
+    image: 'megu.png',
+  },
+  {
+    name: 'Kot',
+    link: 'https://yukata.tech',
+    image: 'kot.png',
+  },
+  {
+    name: 'Sakuradancer',
+    link: 'https://sakuradancer.zone',
+    image: 'sakuradancer.png',
+  },
+];
+
+export default Friends;
