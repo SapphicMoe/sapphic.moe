9d81083ddc9779a09cc03d70003f8bfb3e48b7a4
Chloe <solely@riseup.net>
2023-03-17 23:23:43 +0500

    refactor: switch back to mdx for now

--- astro.config.ts
+++ astro.config.ts
@@ -2,25 +2,24 @@ import { defineConfig } from 'astro/config';
 import type { AstroIntegration } from 'astro';
 
 import mdx from '@astrojs/mdx';
-import markdoc from '@astrojs/markdoc';
 import prefetch from '@astrojs/prefetch';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 import compress from 'astro-compress';
-import autolinkHeadings from 'rehype-autolink-headings';
-import externalLinks from 'rehype-external-links';
-import slug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeExternalLinks from 'rehype-external-links';
+import rehypeSlug from 'rehype-slug';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://arciniega.one',
   markdown: {
     rehypePlugins: [
-      slug,
-      autolinkHeadings,
+      rehypeSlug,
+      rehypeAutolinkHeadings,
       [
-        externalLinks,
+        rehypeExternalLinks,
         {
           target: '_blank',
           rel: ['nofollow', 'noopener'],
@@ -35,7 +34,6 @@ export default defineConfig({
     compress(),
 
     // TEMP: The recent Astro update broke integration logic, so "AstroIntegration" has been supplied here for now.
-    markdoc() as AstroIntegration,
     mdx() as AstroIntegration,
   ],
   output: 'server',
