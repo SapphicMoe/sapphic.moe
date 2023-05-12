974c3b500b8c75a4e2b718a2a2505d89f68f5979
Chloe <solely@riseup.net>
2023-05-12 22:41:03 +0500

    refactor: use /article/ slug instead of post

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -4,16 +4,16 @@ import { blog } from '@config';
 import { getCollection } from 'astro:content';
 import { parseISO } from 'date-fns';
 
-const posts = await getCollection('posts');
+const articles = await getCollection('posts');
 
 export const get = () =>
   rss({
     title: blog.rss.title,
     description: blog.rss.description,
     site: import.meta.env.SITE,
-    items: posts.map((post) => ({
+    items: articles.map((post) => ({
       description: post.data.description,
-      link: `posts/${post.slug}`,
+      link: `article/${post.slug}`,
       pubDate: parseISO(post.data.created),
       title: post.data.title,
     })),
