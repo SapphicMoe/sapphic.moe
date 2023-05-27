dcfbd157953f561015e5be2d4240ead97d24c868
Chloe <solely@riseup.net>
2023-05-27 21:01:12 +0500

    refactor: total revamp!

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,15 +2,27 @@ import { z, defineCollection } from 'astro:content';
 
 const dateRegex = /^[0-9]{4}-[0-9]{2}-[0-9]{2}Z$/;
 
-export const collections = {
-  articles: defineCollection({
-    schema: z.object({
-      title: z.string(),
-      created: z.string().regex(dateRegex),
-      description: z.string(),
-      tag: z.string(),
-      comments: z.boolean().optional().default(true),
-      timeEstimate: z.string().optional(),
-    }),
+const articlesCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    created: z.string().regex(dateRegex),
+    modified: z.string().regex(dateRegex).optional(),
+    tag: z.string(),
+    timeEstimate: z.string().optional(),
+    comments: z.boolean().optional().default(true),
   }),
+});
+
+const infoCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    modified: z.string().regex(dateRegex).optional(),
+  }),
+});
+
+export const collections = {
+  articles: articlesCollection,
+  info: infoCollection,
 };
