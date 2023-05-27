cfacd9a60b0239c69983e8bf336bf4efce0f875b
Chloe <solely@riseup.net>
2023-05-27 20:53:43 +0500

    refactor(tailwind): config file

--- modules.d.ts
+++ modules.d.ts
@@ -1,2 +1,3 @@
 declare module '@fec/remark-a11y-emoji';
 declare module '@microflash/remark-figure-caption';
+declare module '@catppuccin/tailwindcss';
---
+++ tailwind.config.ts
@@ -0,0 +1,98 @@
+import type { Config } from 'tailwindcss';
+import type { PluginAPI } from 'tailwindcss/types/config';
+
+import catppuccin from '@catppuccin/tailwindcss';
+import typography from '@tailwindcss/typography';
+
+export default {
+  content: ['./src/**/*.{astro,html,js,md,mdx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      body: ['Inter', 'sans-serif'],
+      display: ['Atkinson Hyperlegible', 'sans-serif'],
+      mono: ['Iosevka', 'monospace'],
+    },
+    colors: {},
+    extend: {
+      typography: () => ({
+        invert: {
+          css: {
+            '--tw-prose-invert-bullets': '#f38ba8',
+
+            hr: {
+              background: '#f5c2e7',
+              height: '1px',
+              borderTopWidth: '0',
+            },
+            a: {
+              color: '#f5c2e7',
+              textDecorationLine: 'none',
+
+              '&:hover': {
+                color: '#c99fbe',
+                textDecorationLine: 'underline',
+              },
+            },
+            figure: {
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+
+              '& img': {
+                maxHeight: '500px',
+                width: 'auto',
+                height: '100%',
+              },
+            },
+            figcaption: {
+              fontStyle: 'italic',
+              color: 'white',
+              opacity: '0.75',
+            },
+            'p, li': {
+              code: {
+                border: '0.1px solid',
+                padding: '0.1rem 0.65rem',
+                borderRadius: '0.5rem',
+                fontWeight: '400',
+              },
+            },
+          },
+        },
+        DEFAULT: {
+          css: {
+            '--tw-prose-bullets': '#8839ef',
+
+            hr: {
+              background: '#8839ef',
+              height: '1px',
+              borderTopWidth: '0',
+            },
+            a: {
+              color: '#8839ef',
+              textDecorationLine: 'none',
+
+              '&:hover': {
+                color: '#6027ab',
+                textDecorationLine: 'underline',
+              },
+            },
+          },
+        },
+      }),
+    },
+  },
+  plugins: [
+    catppuccin({
+      prefix: 'ctp',
+    }),
+    typography,
+    ({ addComponents }: PluginAPI) => {
+      addComponents({
+        '.title': {
+          '@apply text-4xl font-bold': {},
+        },
+      });
+    },
+  ],
+} satisfies Config;
