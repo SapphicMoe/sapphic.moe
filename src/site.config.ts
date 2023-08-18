9afeb5a0bd7d62d4c07cb38b243ad311ed9891a6
Chloe <solely@riseup.net>
2023-08-19 02:26:09 +0500

    chore(config): clean up comments

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,37 +1,26 @@
 // Base configuration for this website.
-
 export const base = {
   favicon: {
-    // Alt text for the favicon
-    altText: 'A pink lotus flower in full bloom.',
-
-    // The favicon file name (for example, /favicon.png)
-    fileName: '/lotus.png',
+    altText: 'A pink lotus flower in full bloom.', // Alt text for the favicon
+    fileName: '/lotus.png', // The favicon file name (for example, /favicon.png)
   },
-
-  // The site's accent color that will appear in embeds
-  themeColor: '#EF8496',
-
-  // The site's name. Used for Discord/Twitter embeds and etc.
-  siteName: 'arciniega.one',
+  themeColor: '#EF8496', // The site's accent color that will appear in embeds
+  siteName: 'arciniega.one', // The site's name. Used for Discord/Twitter embeds and etc.
 };
 
 // Blog configuration.
-
 export const blog = {
   // Whether to allow GitHub discussion comments provided via Giscus on your blog posts or not.
   // See https://giscus.app/ for more information and edit "src/components/blog/comments.astro" if set to true.
   comments: true,
 
   rss: {
-    // Title for the RSS feed
-    title: "Chloe's Posts",
-
-    // Description for the RSS feed
-    description: 'A personal blog, comprising of whatever stuff I find interesting to post.',
+    title: "Chloe's Posts", // RSS feed title
+    description: 'A personal blog, comprising of whatever stuff I find interesting to post.', // RSS feed description
   },
 };
 
+// Miscellaneous configruation.
 export const misc = {
   // Whether to enable Plausible Analytics.
   // If you're using Vercel, you must also edit the rewrites found in the vercel.json file.
