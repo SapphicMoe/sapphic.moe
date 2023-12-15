edb05e6732816b675315bf74410b8a6330b733ff
Chloe <solely@riseup.net>
2023-12-16 03:11:50 +0600

    chore: disable dev toolbar

--- astro.config.ts
+++ astro.config.ts
@@ -26,6 +26,9 @@ export default defineConfig({
   server: {
     open: true,
   },
+  devToolbar: {
+    enabled: false,
+  },
   markdown: {
     shikiConfig: {
       theme: 'material-theme-ocean',
