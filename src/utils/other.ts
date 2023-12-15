e5497580935fc7caede5f05a3c7a4a58218f0fb1
Chloe <solely@riseup.net>
2023-12-16 03:11:09 +0600

    refactor: add other buttons section

---
+++ src/utils/other.ts
@@ -0,0 +1,185 @@
+import type { OtherButton } from '@typings/utils.type';
+
+const buttons = [
+  {
+    name: '88x31',
+    link: 'https://cyber.dabamos.de/88x31/index.html',
+    image: '88x31.gif',
+  },
+  {
+    name: 'Antifa',
+    image: 'antifa.gif',
+  },
+  {
+    name: 'Antinazi - No Nazi, No Fascism, No Racism',
+    image: 'antinazi.gif',
+  },
+  {
+    name: 'Anime is gay as hell, but I approve!',
+    image: 'animegay.gif',
+  },
+  {
+    name: 'Anything but Chrome',
+    image: 'anythingbut.gif',
+  },
+  {
+    name: 'AOL sucks, get a real ISP',
+    image: 'aol-sucks.gif',
+  },
+  {
+    name: 'Arch Linux',
+    link: 'https://archlinux.org/',
+    image: 'archlinux.gif',
+  },
+  {
+    name: 'Asexuals NOW!',
+    image: 'asexuals_now.gif',
+  },
+  {
+    name: 'Bitwarden',
+    link: 'https://bitwarden.com',
+    image: 'bitwarden.gif',
+  },
+  {
+    name: 'Bunny Browser',
+    image: 'bunbrowser.gif',
+  },
+  {
+    name: 'Click here to CRUSH CAPITALISM',
+    link: "javascript:alert(':D');",
+    image: 'cc-button.gif',
+  },
+  {
+    name: 'Debian Powered',
+    link: 'https://www.debian.org/',
+    image: 'debian.gif',
+  },
+  {
+    name: 'DRM - Defective by Design. Elimate DRM NOW!',
+    link: 'https://www.defectivebydesign.org/',
+    image: 'dbd.gif',
+  },
+  {
+    name: 'elixi.re',
+    link: 'https://elixi.re/',
+    image: 'elixire.gif',
+  },
+  {
+    name: 'Firefox',
+    link: 'https://www.mozilla.org/en-US/firefox/new',
+    image: 'firefox.gif',
+  },
+  {
+    name: 'Foxwells Garden',
+    link: 'https://foxwells.garden/',
+    image: 'foxwells.gif',
+  },
+  {
+    name: 'Geocities',
+    image: 'geocities.gif',
+  },
+  {
+    name: 'Gitea',
+    link: 'https://gitea.io/',
+    image: 'gitea.gif',
+  },
+  {
+    name: 'GIRLS 4 Notepad',
+    image: 'girls4notepad.gif',
+  },
+  {
+    name: 'Google Chrome IS EVIL!',
+    image: 'chrmevil.gif',
+  },
+  {
+    name: 'Half-Life',
+    link: 'https://www.half-life.com/',
+    image: 'half-life.gif',
+  },
+  {
+    name: 'Internet Explorer IS EVIL!',
+    image: 'ieisevil.gif',
+  },
+  {
+    name: "It's okay to give your bros some love",
+    image: 'brolove.gif',
+  },
+  {
+    name: 'Jellyfin',
+    link: 'https://jellyfin.org',
+    image: 'jellyfin.gif',
+  },
+  {
+    name: 'Join the Fediverse',
+    link: 'https://www.fediverse.to/',
+    image: 'fediverse.gif',
+  },
+  {
+    name: 'LaTeX',
+    link: 'https://www.latex-project.org',
+    image: 'latex.gif',
+  },
+  {
+    name: 'Mafumafu',
+    link: 'https://www.youtube.com/c/uni_mafumafu',
+    image: 'mafumafu.gif',
+  },
+  {
+    name: 'Made with Neovim',
+    link: 'https://neovim.io/',
+    image: 'neovim.gif',
+  },
+  {
+    name: 'Mastodon',
+    link: 'https://joinmastodon.org',
+    image: 'mastodon.gif',
+  },
+  {
+    name: 'Non-binary pride!',
+    image: 'nonbinary.gif',
+  },
+  {
+    name: 'Powered by AMD',
+    link: 'https://www.amd.com/',
+    image: 'amd.gif',
+  },
+  {
+    name: 'Powered by Fedora',
+    link: 'https://fedoraproject.org/',
+    image: 'fedora.gif',
+  },
+  {
+    name: 'Powered by SDF',
+    link: 'https://sdf.org/',
+    image: 'sdf.gif',
+  },
+  {
+    name: 'Queer pride!',
+    image: 'queerpride.gif',
+  },
+  {
+    name: 'Soulseek',
+    link: 'https://www.slsknet.org/',
+    image: 'soulseek.gif',
+  },
+  {
+    name: 'This site is Miku APPROVED',
+    image: 'miku.gif',
+  },
+  {
+    name: 'This website is GAY',
+    image: 'bu12.gif',
+  },
+  {
+    name: 'Trans rights NOW!',
+    link: "javascript:alert('🏳️‍⚧️');",
+    image: 'transnow.gif',
+  },
+  {
+    name: 'Trans your gender',
+    link: 'https://diyhrt.wiki/',
+    image: 'tyg.gif',
+  },
+] satisfies OtherButton[];
+
+export default buttons;
