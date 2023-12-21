d69e20b085dd5a5bfdfbef50753f5520c283d5e7
Chloe <solely@riseup.net>
2023-12-22 02:12:42 +0600

    chore(other): more buttons!

--- src/utils/other.ts
+++ src/utils/other.ts
@@ -7,6 +7,18 @@ const buttons = [
     image: '88x31.gif',
   },
   {
+    name: 'All Cops Are Bastards',
+    image: 'acab.gif',
+  },
+  {
+    name: 'Anarchy NOW!',
+    image: 'anarchy.gif',
+  },
+  {
+    name: 'Anime is gay as hell, but I approve!',
+    image: 'animegay.gif',
+  },
+  {
     name: 'Antifa',
     image: 'antifa.gif',
   },
@@ -15,16 +27,12 @@ const buttons = [
     image: 'antinazi.gif',
   },
   {
-    name: 'Anime is gay as hell, but I approve!',
-    image: 'animegay.gif',
-  },
-  {
     name: 'Anything but Chrome',
     image: 'anythingbut.gif',
   },
   {
     name: 'AOL sucks, get a real ISP',
-    image: 'aol-sucks.gif',
+    image: 'aolsucks.gif',
   },
   {
     name: 'Arch Linux',
@@ -33,7 +41,15 @@ const buttons = [
   },
   {
     name: 'Asexuals NOW!',
-    image: 'asexuals_now.gif',
+    image: 'asexualsnow.gif',
+  },
+  {
+    name: 'Autism NOW!',
+    image: 'autismnow.gif',
+  },
+  {
+    name: 'Best viewed with any browser [Anarchy logo in Netscape Navigator style]',
+    image: 'bestviewed.gif',
   },
   {
     name: 'Bitwarden',
@@ -42,12 +58,26 @@ const buttons = [
   },
   {
     name: 'Bunny Browser',
-    image: 'bunbrowser.gif',
+    image: 'bunnybrowser.gif',
+  },
+  {
+    name: 'Canadians NOW!',
+    image: 'canadiansnow.gif',
+  },
+  {
+    name: "Car Seat Headrest's Twin Fantasy album - MUSIC FOR THE GAYS!!",
+    link: 'https://carseatheadrest.bandcamp.com/album/twin-fantasy',
+    image: 'carseatheadrest.gif',
   },
   {
     name: 'Click here to CRUSH CAPITALISM',
     link: "javascript:alert(':D');",
-    image: 'cc-button.gif',
+    image: 'clickheretocrushcapitalism.gif',
+  },
+  {
+    name: 'Coded with Pride',
+    link: 'https://pride.codes',
+    image: 'codedwithpride.gif',
   },
   {
     name: 'Debian Powered',
@@ -57,12 +87,17 @@ const buttons = [
   {
     name: 'DRM - Defective by Design. Elimate DRM NOW!',
     link: 'https://www.defectivebydesign.org/',
-    image: 'dbd.gif',
+    image: 'drm.gif',
   },
   {
     name: 'elixi.re',
     link: 'https://elixi.re/',
     image: 'elixire.gif',
+  },
+  {
+    name: 'FFmpeg',
+    link: 'https://ffmpeg.org/',
+    image: 'ffmpeg.gif',
   },
   {
     name: 'Firefox',
@@ -75,8 +110,18 @@ const buttons = [
     image: 'foxwells.gif',
   },
   {
-    name: 'Geocities',
-    image: 'geocities.gif',
+    name: 'Free Tech Tips - CLICK HERE',
+    link: 'https://linustechtips.com',
+    image: 'freetechtips.gif',
+  },
+  {
+    name: 'Get Quake NOW!',
+    link: 'https://www.quake.com/',
+    image: 'getquakenow.gif',
+  },
+  {
+    name: 'Girls NOW!',
+    image: 'girlsnow.png',
   },
   {
     name: 'Gitea',
@@ -84,25 +129,64 @@ const buttons = [
     image: 'gitea.gif',
   },
   {
-    name: 'GIRLS 4 Notepad',
-    image: 'girls4notepad.gif',
+    name: 'Go bare-pawed NOW!',
+    image: 'gobarepawed.gif',
   },
   {
     name: 'Google Chrome IS EVIL!',
-    image: 'chrmevil.gif',
+    image: 'googlechromeisevil.gif',
+  },
+  {
+    name: 'GrapheneOS',
+    link: 'https://grapheneos.org/',
+    image: 'grapheneos.gif',
   },
   {
     name: 'Half-Life',
     link: 'https://www.half-life.com/',
     image: 'half-life.gif',
   },
+
+  {
+    name: 'I like computer',
+    image: 'ilikecomputer.png',
+  },
   {
     name: 'Internet Explorer IS EVIL!',
     image: 'ieisevil.gif',
   },
   {
+    name: '100 gecs - I only listen to 100 gecs',
+    link: 'https://www.100gecs.com/',
+    image: 'ionly100gecs.gif',
+  },
+  {
+    name: 'Implement RSS NOW!',
+    link: 'https://rss.com/blog/how-do-rss-feeds-work/',
+    image: 'implementrss.png',
+  },
+  {
+    name: 'Internet Archive',
+    link: 'https://archive.org/',
+    image: 'internetarchive.gif',
+  },
+  {
+    name: 'ISO 8601',
+    link: 'https://xkcd.com/1179/',
+    image: 'iso8601.png',
+  },
+  {
+    name: 'I support Right to Repair',
+    link: 'https://en.wikipedia.org/wiki/Right_to_repair',
+    image: 'isupportrtr.png',
+  },
+  {
     name: "It's okay to give your bros some love",
-    image: 'brolove.gif',
+    image: 'itsokay.gif',
+  },
+  {
+    name: 'I wear cute socks!',
+    image: 'iwearcutesocks.gif',
   },
   {
     name: 'Jellyfin',
@@ -112,12 +196,37 @@ const buttons = [
   {
     name: 'Join the Fediverse',
     link: 'https://www.fediverse.to/',
-    image: 'fediverse.gif',
+    image: 'jointhefediverse.gif',
   },
   {
     name: 'LaTeX',
     link: 'https://www.latex-project.org',
     image: 'latex.gif',
+  },
+  {
+    name: 'Libreboot',
+    link: 'https://libreboot.org/',
+    image: 'libreboot.gif',
+  },
+  {
+    name: 'Made with my own two paws',
+    image: 'madewithtwopaws.gif',
+  },
+  {
+    name: 'Made with pride [lesbian pride flag]',
+    image: 'mw_lesbian.gif',
+  },
+  {
+    name: 'Made with pride [non-binary pride flag]',
+    image: 'mw_nb.gif',
+  },
+  {
+    name: 'Made with pride [pride flag]',
+    image: 'mw_queer.gif',
+  },
+  {
+    name: 'Made with pride [trans flag]',
+    image: 'mw_trans.gif',
   },
   {
     name: 'Mafumafu',
@@ -135,27 +244,72 @@ const buttons = [
     image: 'mastodon.gif',
   },
   {
+    name: 'Neocities',
+    link: 'https://neocities.org/',
+    image: 'neocities.gif',
+  },
+  {
+    name: 'No WEBP! Just use PNG',
+    image: 'nowebp.gif',
+  },
+  {
+    name: "Non-binary because I'm not a robot",
+    image: 'nonbinary2.png',
+  },
+  {
     name: 'Non-binary pride!',
     image: 'nonbinary.gif',
   },
   {
+    name: 'Plastic Love NOW!',
+    link: 'https://www.youtube.com/watch?v=T_lC2O1oIew',
+    image: 'plasticlove.gif',
+  },
+  {
     name: 'Powered by AMD',
     link: 'https://www.amd.com/',
-    image: 'amd.gif',
+    image: 'pb_amd.gif',
   },
   {
     name: 'Powered by Fedora',
     link: 'https://fedoraproject.org/',
-    image: 'fedora.gif',
+    image: 'pb_fedora.gif',
+  },
+  {
+    name: 'Powered by FreeBSD',
+    link: 'https://www.freebsd.org/',
+    image: 'pb_freebsd.gif',
   },
   {
     name: 'Powered by SDF',
     link: 'https://sdf.org/',
-    image: 'sdf.gif',
+    image: 'pb_sdf.gif',
+  },
+  {
+    name: 'Privacy NOW!',
+    image: 'privacynow.gif',
+  },
+  {
+    name: 'Programming socks NOW!',
+    image: 'programmingsocks.png',
+  },
+  {
+    name: 'Queer Anime Database',
+    link: 'https://queeranime.neocities.org/',
+    image: 'queeranime.png',
+  },
+  {
+    name: 'Queer coded webring',
+    link: 'https://isaacfish.neocities.org/webring/',
+    image: 'queercoded.png',
   },
   {
     name: 'Queer pride!',
     image: 'queerpride.gif',
+  },
+  {
+    name: 'Sharing is caring... seed your torrents!',
+    image: 'sharingiscaring.gif',
   },
   {
     name: 'Soulseek',
@@ -163,12 +317,26 @@ const buttons = [
     image: 'soulseek.gif',
   },
   {
+    name: 'Sun Microsystems',
+    link: 'https://en.wikipedia.org/wiki/Sun_Microsystems',
+    image: 'sun.gif',
+  },
+  {
+    name: 'System Shock 2',
+    link: 'https://store.steampowered.com/app/238210/System_Shock_2/',
+    image: 'systemshock2.gif',
+  },
+  {
     name: 'This site is Miku APPROVED',
     image: 'miku.gif',
   },
   {
     name: 'This website is GAY',
-    image: 'bu12.gif',
+    image: 'thiswebsiteisgay.gif',
+  },
+  {
+    name: 'Tranarchy',
+    image: 'tranarchy.gif',
   },
   {
     name: 'Trans rights NOW!',
@@ -178,7 +346,32 @@ const buttons = [
   {
     name: 'Trans your gender',
     link: 'https://diyhrt.wiki/',
-    image: 'tyg.gif',
+    image: 'transyourgender.gif',
+  },
+  {
+    name: 'uBlock Origin NOW!',
+    link: 'https://ublockorigin.com/',
+    image: 'ublocknow.png',
+  },
+  {
+    name: 'Vocaloid NOW!',
+    link: 'https://www.vocaloid.com/en',
+    image: 'vocaloid.png',
+  },
+  {
+    name: 'voidtools',
+    link: 'https://voidtools.com/',
+    image: 'voidtools.png',
+  },
+  {
+    name: 'wiby.me search',
+    link: 'https://wiby.me/',
+    image: 'wiby.gif',
+  },
+  {
+    name: 'Yesterweb - Reclaim the Net',
+    link: 'https://yesterweb.org/',
+    image: 'yesterweb.png',
   },
 ] satisfies OtherButton[];
 
