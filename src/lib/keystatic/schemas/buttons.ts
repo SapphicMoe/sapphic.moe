9af94ba3865be7c6d3863ba832eb9aee87744c86
Chloe <solely@riseup.net>
2024-03-25 03:50:47 +0500

    chore: remove other buttons

--- src/lib/keystatic/schemas/buttons.ts
+++ src/lib/keystatic/schemas/buttons.ts
@@ -31,34 +31,4 @@ export default {
       itemLabel: (props) => props.fields.name.value,
     }
   ),
-
-  other: fields.array(
-    fields.object({
-      name: fields.text({
-        label: 'Name',
-        description: 'Name of the button to display.',
-        validation: { length: { min: 1 } },
-      }),
-
-      image: fields.image({
-        label: 'Image',
-        description: 'Image to display.',
-        directory: 'public/buttons/',
-        publicPath: '/buttons/',
-        validation: { isRequired: true },
-      }),
-
-      url: fields.url({
-        label: 'URL',
-        description: 'URL of the link to display.',
-      }),
-    }),
-
-    {
-      label: 'Other buttons',
-      slugField: 'name',
-      description: 'Other buttons to display in the main page',
-      itemLabel: (props) => props.fields.name.value,
-    }
-  ),
 };
