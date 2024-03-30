0c0ab1157bc92fc11cc37683c1f4b7ae2a5e77e5
Chloe <solely@riseup.net>
2024-03-30 23:41:54 +0500

    chore: add basic nix flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+
+{
+  description = "🌸 The website for the Sapphic Angels system. Made with Astro.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    flake-utils,
+    nixpkgs,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs = [
+              nodejs
+              corepack_latest
+            ];
+          };
+      }
+    );
+}
