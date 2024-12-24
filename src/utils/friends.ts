406a511a4d35c04a41294e702eba2fefb9ebcddd
heawer <accounts+ghtest@sapphic.moe>
2024-12-25 00:51:23 +0500

    chore(friends): switch to static objects

---
+++ src/utils/friends.ts
@@ -0,0 +1,108 @@
+interface Friend {
+  name: string;
+  image: string;
+  url: string;
+}
+
+export const friends = [
+  {
+    name: 'Alyxia',
+    image: 'alyxia.png',
+    url: 'https://alyxia.dev',
+  },
+  {
+    name: 'espi',
+    image: 'espi.png',
+    url: 'https://espi.me',
+  },
+  {
+    name: 'adryd',
+    image: 'adryd.png',
+    url: 'https://adryd.com',
+  },
+  {
+    name: 'maia',
+    image: 'maia.png',
+    url: 'https://maia.crimew.gay',
+  },
+  {
+    name: 'Ven',
+    image: 'ven.png',
+    url: 'https://vendicated.dev',
+  },
+  {
+    name: 'Fleepy',
+    image: 'fleepy.png',
+    url: 'https://fleepy.tv',
+  },
+  {
+    name: 'Alula',
+    image: 'alula.png',
+    url: 'https://alula.me',
+  },
+  {
+    name: 'erin',
+    image: 'erin.png',
+    url: 'https://e2.pm',
+  },
+  {
+    name: 'Cynthia',
+    image: 'cynthia.png',
+    url: 'https://c7.pm',
+  },
+  {
+    name: 'UtsuhoRocks',
+    image: 'utsuhorocks.png',
+    url: 'https://utsuho.rocks',
+  },
+  {
+    name: 'Vin',
+    image: 'vin.png',
+    url: 'https://vin.pet',
+  },
+  {
+    name: 'Ovyerus',
+    image: 'ovyerus.png',
+    url: 'https://ovyerus.com',
+  },
+  {
+    name: 'lewisakura',
+    image: 'lewisakura.png',
+    url: 'https://lewisakura.moe',
+  },
+  {
+    name: 'Spotlight',
+    image: 'spotlight.gif',
+    url: 'https://joscomputing.space',
+  },
+  {
+    name: 'ziad87',
+    image: 'ziad87.gif',
+    url: 'https://ziad87.net',
+  },
+  {
+    name: 'auravoid',
+    image: 'auravoid.png',
+    url: 'https://auravoid.dev',
+  },
+  {
+    name: 'zptr',
+    image: 'zptr.gif',
+    url: 'https://zptr.cc',
+  },
+  {
+    name: 'cin',
+    image: 'cin.png',
+    url: 'https:///hytracer.ink',
+  },
+  {
+    name: 'pihnea',
+    image: 'pihnea.png',
+    url: 'https://botoaca.github.io',
+  },
+  {
+    name: 'resolv',
+    image: 'resolv.png',
+    url: 'https://resolv.moe',
+  },
+] satisfies Friend[];
