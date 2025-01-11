f16ac5c7883d542cd235e9b3e3c2671cfe76fe35
Chloe <chloe@sapphic.moe>
2025-01-11 06:02:08 +0500

    chore: maybe another time

--- src/pages/article/[...id].png.ts
+++ src/pages/article/[...id].png.ts
@@ -6,12 +6,6 @@ import { Resvg } from '@resvg/resvg-js';
 import { getCollection } from 'astro:content';
 import { format } from 'date-fns';
 
-import HTMLTemplate from '$components/blog/OGImage.astro';
-
-import { getContainerRenderer } from '@astrojs/mdx';
-import { loadRenderers } from 'astro:container';
-import { experimental_AstroContainer as AstroContainer } from 'astro/container';
-
 import satori from 'satori';
 import Atkinson from '$utils/fonts/AtkinsonHyperlegible-Regular.ttf';
 const dimensions = {
@@ -46,19 +40,35 @@ export const GET = async (context: APIContext) => {
   const { title, description, created } = context.props as Props;
   const createdAt = format(new Date(created), 'MMMM d, yyyy');
 
-  const container = await AstroContainer.create({
-    renderers: await loadRenderers([getContainerRenderer()]),
-  });
+  const imageTemplate = html`
+    <div class="flex h-full w-full flex-col bg-[#1e1e2e] text-[#cdd6f4]">
+      <div class="flex h-4/5 w-full flex-col justify-center p-10">
+        <span class="text-xl	text-[#9399b2]">${createdAt}</span>
+        <h1 class="text-6xl font-bold">${title}</h1>
+        <h2 class="opacity-60">${description}</h2>
+      </div>
 
-  const htmlTemplate = await container.renderToString(HTMLTemplate, {
-    props: {
-      title,
-      description,
-      created: createdAt
-    },
-  });
-
-  const imageTemplate = html(htmlTemplate);
+      <div class="flex h-1/5 w-full items-center justify-between border-t-4 border-[#f5c2e7] p-10">
+        <div class="flex items-center">
+          <img class="h-15 w-15 rounded-full" src="https://github.com/SapphicMoe.png" />
+          <div class="flex flex-col pl-3">
+            <span class="text-2xl">Sapphic Angels</span>
+            <span class="text-[#f5c2e7]">(she/it/they - plural)</span>
+          </div>
+        </div>
+        <div class="flex items-center">
+          <img
+            class="h-10 w-10"
+            src="https://em-content.zobj.net/source/google/387/cherry-blossom_1f338.png"
+          />
+          <div class="flex flex-row items-center pl-3 text-2xl">
+            <span>Article on</span>
+            <span class="pl-1.5 text-[#f5c2e7]">sapphic.moe</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
 
   const svg = await satori(imageTemplate as ReactNode, {
     fonts: [
