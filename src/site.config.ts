d08d4c7e170fdee3d130f09b08adc09c87536c9f
Chloe <chloe@sapphic.moe>
2025-01-18 03:22:25 +0500

    chore: fly.io cors testing

--- src/site.config.ts
+++ src/site.config.ts
@@ -44,7 +44,7 @@ export const blog: Config['blog'] = {
       'data-reactions-enabled': '1',
       'data-emit-metadata': '0',
       'data-input-position': 'top',
-      'data-theme': 'https://sapphic.fly.dev/giscus.css',
+      'data-theme': 'https://sapphic.moe/giscus.css',
       'data-lang': 'en',
       crossorigin: 'anonymous',
     },
